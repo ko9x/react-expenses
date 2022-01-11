fccb49e4235dd3846ccfef733c34485dbffdc913
Sean McGillis <slcidevice@yahoo.com>
2022-01-11 16:24:09 -0700

    Created a controlled component named ExpensesFilter. The data is passed between the new component and the Expenses component.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
+import { useState } from "react"
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-import Card from '../UI/Card';
-
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 // You can write the functions in this style
 
@@ -34,30 +35,37 @@ import Card from '../UI/Card';
 
 // Or in the FAT ARROW style
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2020");
+  const changeYear = (passedInYear) => {
+    setSelectedYear(passedInYear);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={selectedYear} onChangeYear={changeYear}/>  
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
