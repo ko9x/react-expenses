babb96ec91d611d1b0071264c8bdc16b4b0cb839
Sean McGillis <slcidevice@yahoo.com>
2022-01-10 21:41:52 -0700

    added a comment showing the different ways to write a function in react

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,38 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from '../UI/Card';
 
-function Expenses(props) {
+
+// You can write the functions in this style
+
+// function Expenses(props) {
+//   return (
+//     <Card className="expenses">
+//       <ExpenseItem
+//         title={props.items[0].title}
+//         amount={props.items[0].amount}
+//         date={props.items[0].date}
+//       />
+//       <ExpenseItem
+//         title={props.items[1].title}
+//         amount={props.items[1].amount}
+//         date={props.items[1].date}
+//       />
+//       <ExpenseItem
+//         title={props.items[2].title}
+//         amount={props.items[2].amount}
+//         date={props.items[2].date}
+//       />
+//       <ExpenseItem
+//         title={props.items[3].title}
+//         amount={props.items[3].amount}
+//         date={props.items[3].date}
+//       />
+//     </Card>
+//   );
+// }
+
+// Or in the FAT ARROW style
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
