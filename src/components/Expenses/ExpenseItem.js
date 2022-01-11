8beb00856040166e80da7c9a7fc9279bb2ad8978
Sean McGillis <slcidevice@yahoo.com>
2022-01-11 16:42:47 -0700

    cleaned up unused code and removed some comments that made no sense once the unused code was removed.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -22,22 +20,13 @@ import Card from '../UI/Card';
 // Or in this style
 function ExpenseItem(props) {
 
-  // the convention is to use a variable name that describes the value, such as title and then to name the function "set" + "variable name". So in this instance the value is "title " and the function is setTitle. Also using "const" is the convention when deconstructing an array.
-  const [title, setTitle] = useState(props.title);
-
-  // Naming your function with the "event type" and then the word "Handler" is the convention in react. So in this case the "onClick" type function I created is named clickHandler
-  const clickHandler = () => {
-    setTitle('this is the title');
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} /> 
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
