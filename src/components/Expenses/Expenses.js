a5c138270396db0f71c8135df223ea7575ac221b
Sean McGillis <slcidevice@yahoo.com>
2022-01-12 16:13:13 -0700

    Refactored to allow users to add new expenses. There is a bug where the date shown in the list is 1 day before the date selected

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
@@ -39,30 +39,18 @@ const Expenses = (props) => {
   const changeYear = (passedInYear) => {
     setSelectedYear(passedInYear);
   };
+  
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={selectedYear} onChangeYear={changeYear}/>  
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpensesFilter selected={selectedYear} onChangeYear={changeYear} />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
